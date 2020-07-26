217c3f4d772373399a9d718f9a0c3cd8024f2bb0
Dieter <dieter.decaestecker@test.com>
2020-07-26 13:45:20 +0200

    No magic number

--- problem6/problem6.py
+++ problem6/problem6.py
@@ -13,7 +13,9 @@ def sum_squares(number_of_terms):
 
 
 if __name__ == "__main__":
-    square_of_sum = math.pow(gauss_series(100), 2)
-    sum_of_squares = sum_squares(100)
+    number_of_terms = 100
+
+    square_of_sum = math.pow(gauss_series(number_of_terms), 2)
+    sum_of_squares = sum_squares(number_of_terms)
 
     print(math.fabs(sum_of_squares - square_of_sum))
