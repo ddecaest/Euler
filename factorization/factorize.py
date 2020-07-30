7516fc0f76900ed1eee922301e0a700759c365a3
Dieter <dieter.decaestecker@test.com>
2020-07-30 20:30:03 +0200

    Solved problem 12

--- factorization/factorize.py
+++ factorization/factorize.py
@@ -8,6 +8,51 @@ from primetest import miller_rabin
 first_prime = 2
 
 
+class CachedFactorizer:
+    cached_primes = [2]
+
+    def factorize_number(self, number):
+        if number <= 0:
+            return []
+        if number == 1:
+            # A bit of a weird case, 1 is technically not a prime number...
+            return [(1, 1)]
+
+        primes = []
+        defactorized = number
+
+        for cached_prime in self.cached_primes:
+            power, defactorized = _find_power_of_prime_divisor(cached_prime, defactorized)
+            if power != 0:
+                primes.append((cached_prime, power))
+            if defactorized == 1:
+                return primes
+
+        biggest_cached_prime = self.cached_primes[len(self.cached_primes) - 1]
+        for factor_candidate in range(biggest_cached_prime + 1, number + 1):
+            is_prime = miller_rabin.prime_test(factor_candidate, 40)
+            if is_prime:
+                self.cached_primes.append(factor_candidate)
+
+                power, defactorized = _find_power_of_prime_divisor(factor_candidate, defactorized)
+                if power != 0:
+                    primes.append((cached_prime, power))
+                if defactorized == 1:
+                    return primes
+
+        return primes
+
+
+def _find_power_of_prime_divisor(prime, number):
+    defactorized = number
+    power = 0
+    while defactorized % prime == 0:
+        power += 1
+        defactorized = defactorized / prime
+
+    return power, defactorized
+
+
 # Returns the prime factorization as list of tuples containing prime number - power, ordered from smallest prime to
 # largest. For example 18 = 2*3*3, so it would return [(2,1),(3,2)]
 def factorize_number(number):
@@ -23,13 +68,9 @@ def factorize_number(number):
     for factor_candidate in range(first_prime, number + 1):
         is_prime = miller_rabin.prime_test(factor_candidate, 40)
         if is_prime:
-            power = 0
-            while defactorized % factor_candidate == 0:
-                power += 1
-                defactorized = defactorized / factor_candidate
+            power, defactorized = _find_power_of_prime_divisor(factor_candidate, defactorized)
             if power != 0:
                 primes.append((factor_candidate, power))
-
         if defactorized == 1:
             break
 
---
+++ problems/problem12.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+scriptpath = "../"
+sys.path.append(os.path.abspath(scriptpath))
+
+from factorization import factorize
+
+# TODO: https://www.mathblog.dk/triangle-number-with-more-than-500-divisors/
+
+def calculate_number_divisors(number):
+    if number < 0:
+        raise Exception("Input must be a positive number")
+    if number == 0:
+        # You can argue for either infinite or 0. Not really relevant...
+        return 0
+
+    factorization = factorize.CachedFactorizer().factorize_number(number)
+    tally = 1
+    for (prime_number, power) in factorization:
+        tally = tally * (power+1)
+    return tally
+
+
+def generate_triangle_number(term_number):
+    # 1 + 2 + 3 + 4 -> Arithmetic sequence
+    return int(term_number * (term_number+1)/2)
+
+
+if __name__ == "__main__":
+    number_of_divisors_required = 500
+
+    candidate_number = 1
+    jump = 2
+    candidate_divisors = calculate_number_divisors(candidate_number)
+
+    while candidate_divisors < number_of_divisors_required:
+        candidate_number += jump
+        jump += 1
+
+        candidate_divisors = calculate_number_divisors(candidate_number)
+
+    print(str(candidate_number) + " -> " + str(candidate_divisors))
